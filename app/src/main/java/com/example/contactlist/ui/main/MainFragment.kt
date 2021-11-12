6ccdc32ff62bbac397e2928102b47afc5d32498e
Dmitry <klochkov.dm595@yandex.ru>
2021-11-12 14:21:36 +0300

    Init commit + binding

---
+++ app/src/main/java/com/example/contactlist/ui/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.example.contactlist.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.contactlist.R
+import com.example.contactlist.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() =_binding!!
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+}
