3b4eae4f33a2f78cbd78f9f142debfb820529f4d
Dmitry <klochkov.dm595@yandex.ru>
2021-11-13 17:58:37 +0300

    Check Permissions, изспользуя requestPermissions()

--- app/src/main/java/com/example/contactlist/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/contactlist/ui/main/MainFragment.kt
@@ -1,11 +1,15 @@
 package com.example.contactlist.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.example.contactlist.R
 import com.example.contactlist.databinding.MainFragmentBinding
 
@@ -27,6 +31,71 @@ class MainFragment : Fragment() {
         _binding = MainFragmentBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        context?.let {
+            when {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.READ_CONTACTS
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    getContacts()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
+                    AlertDialog.Builder(it)
+                        .setTitle("Доступ к контактам")
+                        .setMessage("Объяснение")
+                        .setPositiveButton("Предоставить доступ") { _, _ ->
+                            requestPermission()
+                        }
+                        .setNegativeButton("Не надо") { dialog, _ -> dialog.dismiss() }
+                        .create()
+                        .show()
+                }
+                else -> requestPermission()
+            }
+        }
+    }
+
+    val REQUEST_CODE = 42
+
+    private fun requestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_CODE ->{
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getContacts()
+                } else {
+                    context?.let {
+                        AlertDialog.Builder(it)
+                            .setTitle("Доступ к контактам")
+                            .setMessage("Объяснение")
+                            .setNegativeButton("Не надо") { dialog, _ -> dialog.dismiss() }
+                            .create()
+                            .show()
+                    }
+                    return
+                }
+            }
+        }
+    }
+
+    private fun getContacts() {
+        TODO("Not yet implemented")
+    }
+
 
     override fun onDestroyView() {
         _binding = null
